0441cc0def5ae962c20a9506ad3076e4496c648c
TechCorp07 <poso.draxy@gmail.com>
2022-03-21 17:09:29 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,14 @@
 /**
  * _puts - prints a string
  * @str: pointer string variable
+ * @i: integer variable
  * Return: Always run zero
  */
 
 void _puts(char *str)
 {
+	int i;
+
 	for (i = 0; i < strlen(str); i++)
 			_putchar(str[i]);
 }
