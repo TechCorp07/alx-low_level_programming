2f043545eefd3ecca60d4978b3d124b079922c84
TechCorp07 <poso.draxy@gmail.com>
2022-03-23 14:51:40 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,11 +7,11 @@
  * @n: integer
  */
 
-char *_strncpy(char *dest, char *src, unsigned long n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	unsigned long i;
-	
-	for (i = 0; i < n && src[i] != '\0';i++)
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
